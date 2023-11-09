c85d9e35bfa3ceb8d968110ebc2428c0f51cddc0
Travis Snapp <traviswsnapp@outlook.com>
2023-11-08 22:13:06 -0500

    server connected

--- api/server.js
+++ api/server.js
@@ -1,2 +1,22 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require("express");
+const postRouter = require("./posts/posts-router");
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+
+server.get("/", (req, res) => {
+  res.send("hopefully this messaaage goes through");
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // require your server and launch it here
+
+const server = require("./api/server");
+
+const Port = 6000;
+
+server.get("*", (req, res) => {
+  res.send("hopefully this messaaage goes through");
+});
+
+server.listen(Port, () => {
+  console.log("server is listeing on", Port);
+});
